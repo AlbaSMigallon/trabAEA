06f87028df17573f1a6e4a02e9075249022d1e7f
Mojito <ecanizaresj99@gmail.com>
2023-05-20 17:29:44 +0200

    Añado el metodo terminado Sin Vocales

--- src/SinVocales.java
+++ src/SinVocales.java
@@ -1,4 +1,21 @@
 
-public class SinVocales {
+//SinVocales contendrá un método quitará las vocales de una frase. La frase será un atributo de
+//la clase que se le dará valor en el constructor. No utilizar el método replace().
+import java.util.*;
 
-}
+public class SinVocales {
+	private String frase = "Hola este es nuestro trabajo";
+
+	public String quitarVocales() {
+		String fraseSinVocales = "";
+
+		for (int i = 0; i < frase.length(); i++) {
+			char c = frase.charAt(i);
+			if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+				fraseSinVocales += c;
+			}
+		}
+
+		return fraseSinVocales;
+	}
+}// en el main se puede utilizar el método quitarVocales de la clase SinVocales
